473a186a5261a5fc8b3de1238131dd1c591ea424
Immaculate-Tech <adekoyaaromolaran@gmail.com>
2025-11-07 13:54:05 +0100

    Add files via upload

---
+++ menujava.js
@@ -0,0 +1,15 @@
+function toOpen(){
+    document.getElementById('menu').style.display = 'block';
+
+    document.getElementById('ope').style.display = 'none';
+
+    document.getElementById('clo').style.display = 'block';
+}
+
+function toClose(){
+    document.getElementById('menu').style.display = 'none';
+
+    document.getElementById('ope').style.display = 'block';
+
+    document.getElementById('clo').style.display = 'none';
+}
